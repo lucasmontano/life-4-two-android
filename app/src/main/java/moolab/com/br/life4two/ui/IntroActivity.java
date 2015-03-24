c0af7e372cc1228f50b7bd511e68aeb2c76c2478
Lucas Montano <lucasmontano@gmail.com>
2015-03-23 22:47:29 -0300

    Introduction starting Digits

--- app/src/main/java/moolab/com/br/life4two/ui/IntroActivity.java
+++ app/src/main/java/moolab/com/br/life4two/ui/IntroActivity.java
@@ -1,11 +1,15 @@
 package moolab.com.br.life4two.ui;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.digits.sdk.android.AuthCallback;
+import com.digits.sdk.android.Digits;
 import com.digits.sdk.android.DigitsAuthButton;
 import com.digits.sdk.android.DigitsException;
 import com.digits.sdk.android.DigitsSession;
@@ -13,11 +17,14 @@ import com.parse.LogInCallback;
 import com.parse.ParseAnonymousUtils;
 import com.parse.ParseException;
 import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+import com.squareup.picasso.Picasso;
 import com.twitter.sdk.android.Twitter;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import butterknife.OnClick;
 import io.fabric.sdk.android.Fabric;
 import moolab.com.br.life4two.R;
 import moolab.com.br.life4two.parsecloud.ParseConfigMaster;
@@ -28,8 +35,11 @@ public class IntroActivity extends ActionBarActivity {
     private static final String TWITTER_KEY = "HFLtQqct9AIkG2WMptq2Yw8fP";
     private static final String TWITTER_SECRET = "rzJyxwDQrfKlWlbaGxLklFgzSJtOWl89vgIoFx5VvdtwjjbfVp";
 
-    @InjectView(R.id.auth_button)
-    DigitsAuthButton digitsButton;
+    @InjectView(R.id.logo)
+    ImageView logo;
+
+    @InjectView(R.id.bg)
+    ImageView bg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,23 +47,28 @@ public class IntroActivity extends ActionBarActivity {
 
         TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
         Fabric.with(this, new Twitter(authConfig));
+
         setContentView(R.layout.activity_intro);
 
         ButterKnife.inject(this);
 
-        digitsButton.setCallback(new AuthCallback() {
+        Picasso.with(this).load(R.drawable.fundo).into(bg);
+        Picasso.with(this).load(R.drawable.logo).into(logo);
+    }
+
+    @OnClick(R.id.btn_start)
+    public void onAuthNumber() {
+        AuthCallback callback = new AuthCallback() {
             @Override
-            public void success(DigitsSession session, final String phoneNumber) {
-                ParseAnonymousUtils.logIn(new LogInCallback() {
+            public void success(final DigitsSession session, final String phoneNumber) {
+
+                ParseUser.logInInBackground(String.valueOf(session.getId()), phoneNumber, new LogInCallback() {
                     @Override
                     public void done(ParseUser parseUser, ParseException e) {
-
-                        if (parseUser == null) {
+                        if (e != null) {
+                            signupParseUser(phoneNumber, session.getId());
                             return;
                         }
-
-                        parseUser.put(ParseConfigMaster.USER_KEY_PHONENUMBER, phoneNumber);
-                        parseUser.saveEventually();
                     }
                 });
             }
@@ -61,6 +76,24 @@ public class IntroActivity extends ActionBarActivity {
             @Override
             public void failure(DigitsException exception) {
 
+            }
+        };
+        Digits.authenticate(callback, R.style.CustomDigitsTheme);
+    }
+
+    private void signupParseUser(String phoneNumber, long id) {
+        ParseUser user = new ParseUser();
+        user.setUsername(String.valueOf(id));
+        user.setPassword(phoneNumber);
+        user.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e != null) {
+                    return;
+                }
+
+                startActivity(new Intent(IntroActivity.this, MainActivity.class));
+                finish();
             }
         });
     }
