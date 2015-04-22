ebba0459a5bad5e1a10fe90ffb7d8026ad8cfc4d
Lucas Montano <lucasmontano@MacBook-Air-de-Lucas.local>
2015-04-21 22:01:36 -0300

    Change Invitation entity in Parse, accepting and rejecting invitations

--- app/src/main/java/moolab/com/br/life4two/ui/MainActivity.java
+++ app/src/main/java/moolab/com/br/life4two/ui/MainActivity.java
@@ -5,16 +5,23 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.nispok.snackbar.Snackbar;
+import com.parse.ParseException;
+import com.parse.ParseObject;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import moolab.com.br.life4two.R;
 import moolab.com.br.life4two.core.Cupid;
+import moolab.com.br.life4two.parsecloud.ParseKeysMaster;
 import moolab.com.br.life4two.ui.fragments.InviteFragment;
 import moolab.com.br.life4two.ui.fragments.InvitedFragment;
 import moolab.com.br.life4two.ui.fragments.ReadyFragment;
 import moolab.com.br.life4two.ui.fragments.WaitingFragment;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements InviteFragment.Callback, InvitedFragment.Callback {
+
+    private Cupid cupid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,45 +29,55 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
 
-            Cupid cupid = new Cupid(this);
-            cupid.getStatus(ParseUser.getCurrentUser(), new Cupid.CupidStatus() {
-                @Override
-                public void isInvited() {
-                    navToInvited();
-                }
-
-                @Override
-                public void isEmpty() {
-                    navToInvite();
-                }
-
-                @Override
-                public void isWaiting() {
-                    navToWaiting();
-                }
-
-                @Override
-                public void isReady() {
-                    navToReady();
-                }
-            });
+            loadStatus();
         }
     }
 
+    private void loadStatus() {
+        cupid = new Cupid(this);
+        cupid.getStatus(ParseUser.getCurrentUser(), new Cupid.CupidStatus() {
+            @Override
+            public void isInvited() {
+                navToInvited();
+            }
+
+            @Override
+            public void isEmpty() {
+                navToInvite();
+            }
+
+            @Override
+            public void isWaiting() {
+                navToWaiting();
+            }
+
+            @Override
+            public void isReady() {
+                navToReady();
+            }
+        });
+    }
+
     private void navToWaiting() {
-        getSupportFragmentManager().beginTransaction().add(R.id.container, new WaitingFragment()).commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new WaitingFragment()).commit();
     }
 
     private void navToInvited() {
-        getSupportFragmentManager().beginTransaction().add(R.id.container, new InvitedFragment()).commit();
+
+        Bundle args = new Bundle();
+        args.putString(ParseKeysMaster.SENT_BY, cupid.getInvitedBy().getUsername());
+
+        InvitedFragment fragment = new InvitedFragment();
+        fragment.setArguments(args);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
     }
 
     private void navToReady() {
-        getSupportFragmentManager().beginTransaction().add(R.id.container, new ReadyFragment()).commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new ReadyFragment()).commit();
     }
 
     private void navToInvite() {
-        getSupportFragmentManager().beginTransaction().add(R.id.container, new InviteFragment()).commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new InviteFragment()).commit();
     }
 
 
@@ -75,5 +92,34 @@ public class MainActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onInviteSent() {
+        loadStatus();
+    }
+
+    @Override
+    public void onAcceptInvitation() {
+        cupid.saveStatus(Cupid.ACCEPTED, new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e != null) return;
+                Snackbar.with(getApplicationContext()).text(getString(R.string.msg_accept_done)).show(MainActivity.this);
+                loadStatus();
+            }
+        });
+    }
+
+    @Override
+    public void onRejectInvitation() {
+        cupid.saveStatus(Cupid.REJECTED, new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e != null) return;
+                Snackbar.with(getApplicationContext()).text(getString(R.string.msg_reject_done)).show(MainActivity.this);
+                loadStatus();
+            }
+        });
     }
 }
--- app/src/main/java/moolab/com/br/life4two/ui/fragments/InviteFragment.java
+++ app/src/main/java/moolab/com/br/life4two/ui/fragments/InviteFragment.java
@@ -1,5 +1,6 @@
 package moolab.com.br.life4two.ui.fragments;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -7,12 +8,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 
+import com.nispok.snackbar.Snackbar;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseObject;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
 import moolab.com.br.life4two.R;
+import moolab.com.br.life4two.core.Cupid;
 import moolab.com.br.life4two.parsecloud.ParseKeysMaster;
 
 /**
@@ -23,7 +30,23 @@ public class InviteFragment extends Fragment {
     @InjectView(R.id.boo_phone)
     EditText mBooPhone;
 
+    private Callback mCallback;
+
     public InviteFragment() {
+    }
+
+    public interface Callback {
+        public void onInviteSent();
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mCallback = (Callback) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString() + " must implement InviteFragment.Callback");
+        }
     }
 
     @Override
@@ -35,8 +58,19 @@ public class InviteFragment extends Fragment {
 
     @OnClick(R.id.action_invite)
     public void onInvite() {
-        ParseUser currentUser = ParseUser.getCurrentUser();
-        currentUser.put(ParseKeysMaster.BOO_PHONE, mBooPhone.getText().toString());
-        currentUser.saveInBackground();
+        ParseObject invite = new ParseObject(ParseKeysMaster.OBJECT_INVITE);
+        invite.put(ParseKeysMaster.SENT_TO_PHONE, mBooPhone.getText().toString());
+        invite.put(ParseKeysMaster.SENT_BY, ParseUser.getCurrentUser());
+        invite.put(ParseKeysMaster.STATUS, Cupid.WAITING);
+        invite.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e != null) {
+                    return;
+                }
+                mCallback.onInviteSent();
+                Snackbar.with(getActivity().getApplicationContext()).text(getString(R.string.msg_invite_done)).show(getActivity());
+            }
+        });
     }
 }
